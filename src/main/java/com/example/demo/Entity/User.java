60a3f6ff5f2dbbbedea00f60599dccd0280f959e
Nguyễn Tiến Đạt <datnt2@savisgroup.vn>
2023-07-15 09:23:01 +0700

    thay doi db

--- src/main/java/com/example/demo/Entity/User.java
+++ src/main/java/com/example/demo/Entity/User.java
@@ -5,7 +5,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="users")
+@Table(name="user")
 public class User {
   @Id
   Integer id;
--- src/main/java/com/example/demo/Repository/UserRepository.java
+++ src/main/java/com/example/demo/Repository/UserRepository.java
@@ -11,19 +11,19 @@ import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-    @Query(value = "SELECT * from users", nativeQuery = true)
+    @Query(value = "SELECT * from user", nativeQuery = true)
     List<User> findALlUser();
 
-    @Query(value = "select * from users where id = :id", nativeQuery = true)
+    @Query(value = "select * from user where id = :id", nativeQuery = true)
     List<User> findUserById(@Param("id") String id);
 
     @Modifying
     @Transactional
-    @Query(value = "insert into users(id, name) values (:id, :name)", nativeQuery = true)
+    @Query(value = "insert into user(id, name) values (:id, :name)", nativeQuery = true)
     Integer addNewUser(@Param("id") String id, @Param("name") String name);
 
     @Modifying
     @Transactional
-    @Query(value = "update users set name = :name where id = :id", nativeQuery = true)
+    @Query(value = "update user set name = :name where id = :id", nativeQuery = true)
     Integer updateUser(@Param("name") String name, @Param("id") String id);
 }
