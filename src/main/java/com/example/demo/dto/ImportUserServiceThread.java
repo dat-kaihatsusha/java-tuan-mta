507849dc903b421e58a061642ecda3a2038e1b94
nguyendatmta <nguyendatmta@gmail.com>
2023-07-19 15:18:54 +0700

    import 1 billion records

--- src/main/java/com/example/demo/dto/ImportUserServiceThread.java
+++ src/main/java/com/example/demo/dto/ImportUserServiceThread.java
@@ -2,9 +2,6 @@ package com.example.demo.dto;
 
 import com.example.demo.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-import com.example.demo.dto.UserDTO;
 
 public class ImportUserServiceThread implements Runnable {
 
